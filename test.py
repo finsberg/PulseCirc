15073297c4006deabc7cd5b89546ff1b04e65f4e
root <mjsadeghinia@outlook.com>
2024-04-11 15:56:51 +0000

    a test function based on inflate and contract functions

--- test.py
+++ test.py
@@ -14,40 +14,40 @@ class ThinSphere:
     """
 
     def __init__(self, params):
-        self.r0=params["radius"]
-        self.t0=params["thickness"]
+        
+        self.p = 0
+        self.r=params["radius"]
+        self.t=params["thickness"]
         self.E=params["young_modulus"]
         self.nu=params["poisson_ratio"]
         
-        self.sigma = 0
-        self.p = 0
-        self.r = self.r0
-        self.t = self.t0
-        
-    def _compute_stress_from_activation(self, activation: float):
-        self.sigma = activation * 2
-
-    def contract(self, activation: float) -> float:
-        self._compute_stress_from_activation(activation)
-        
-        r0 = self.r0
-        t0 = self.t0
+    def inflate(self, pressure: float):
+        r=self.r
+        t=self.t
         E = self.E
         nu = self.nu
-        sigma = self.sigma
-
-        # Calculate the pressure from the stress
-        self.p = 2 * t0 * sigma / r0
-        
         # Calculate strain and change in radius
+        sigma=pressure*r/(2*t)
+        eps=sigma/E
+        dr=eps*r
+        # Update the pressure, radius, and thickness  
+        self.r=r+dr
+        self.t=t-nu*dr
+        self.p=pressure
+                 
+    def contract(self, activation: float):
+        r=self.r
+        t=self.t
+        E = self.E
+        nu = self.nu
+        # Calculate strain and change in radius
+        sigma=activation
         eps = sigma / E
-        dr = eps * r0
-        r_new = r0 - dr
-
+        dr = eps * r
         # Update the radius, and thickness
-        self.r = r_new
-        self.t = t0 - nu * dr
-    
+        self.r = r - dr
+        self.t = t - nu * dr
+        
     def get_pressure(self):
         return self.p
     
@@ -56,22 +56,41 @@ class ThinSphere:
         volume=4/3*np.pi*r**3 
         return volume
 
-    def dV_dP(self): 
-        pass
+    def dv_dp(self):
+        r0=self.r
+        t0=self.t
+        p0=self.p
+        v0=self.get_volume()
+        
+        p=p0*(1+1e-6)
+        self.inflate(p)
+        v=self.get_volume()
+        
+        dv_dp=(v-v0)/(p-p0)
+        
+        self.r=r0
+        self.t=t0
+        self.p=p0
+        return dv_dp
         
     def save(self, t: float):
         pass
-        #reults_u, p = self.problem.state.split(deepcopy=True)
-        #reults_u.t=t+1
-        #with dolfin.XDMFFile(outname.as_posix()) as xdmf:
-        #    xdmf.write_checkpoint(reults_u, "u", float(t+1), dolfin.XDMFFile.Encoding.HDF5, True)
-
 
 #%%
-activation=np.arange(0,10,1)
-time=np.arange(0,1,.1)
-0
+activation=np.linspace(0,0.1,100)
+pressure=np.linspace(0,.015,100)
+
 # Units [mm], [N], [MPa]
-params={"radius": 20,"thickness": 2,"young_modulus": 50, "poisson_ratio": 0.3}
+params={"radius": 50,"thickness": 5,"young_modulus": 50, "poisson_ratio": 0.3}
 model=ThinSphere(params)
+vols=[]
+pres=[]
+for (a,p) in zip(activation,pressure):
+    model.contract(a)
+    model.inflate(p)
+    pres.append(model.get_pressure())
+    vols.append(model.get_volume())
+
+import matplotlib.pyplot as plt   
+plt.plot(vols,pres)
 #%%
