ffb23ebe2ea07038df8c7eae0ef451db84db9558
root <mjsadeghinia@outlook.com>
2024-04-18 08:17:39 +0000

    derivation added, and some small changes

--- heart_model_pulse.py
+++ heart_model_pulse.py
@@ -44,7 +44,44 @@ class HeartModelPulse:
         return volume_current
     
     
-    def compute_volume_derivation(self, activation_value: float, pressure_value: float) -> float: ...
+    def compute_volume_derivation(self, activation_value: float, pressure_value: float) -> float: 
+        """
+        Computes dV/dP, with V is the volume of the model and P is the pressure.
+        The derivation is computed as the change of volume due to a small change in the pressure at a given pressure.
+        After computation the problem is reset to its initial state.
+        
+        NB! We use problem.solve() instead of pulse.iterate.iterate, as the pressure change is small and iterate may fail.
+
+        Parameters:
+        activation_value (float): The activation value to be applied.
+        pressure_value (float): The pressure value to be applied.
+
+        Returns:
+        float: The computed dV/dP .
+        """ 
+        # Backing up the problem 
+        state_backup = self.problem.state.copy(deepcopy=True)
+        pressure_backup=float(self.lv_pressure)
+        # Update the problem with the give activation and pressure and store the initial State of the problem 
+        self.lv_pressure.assign(pressure_value)
+        self.activation.assign(activation_value)
+        self.problem.solve()
+        p_i=self.get_pressure()
+        v_i=self.get_volume()
+        
+        # small change in pressure and computing the volume
+        p_f = p_i + 0.001
+        self.lv_pressure.assign(p_f)
+        self.problem.solve()
+        v_f=self.get_volume()
+        
+        dV_dP=(v_f-v_i)/(p_f-p_i)
+        
+        # reset the problem to its initial state
+        self.problem.state.assign(state_backup)
+        self.lv_pressure.assign(pressure_backup)
+        
+        return dV_dP
         
     def get_pressure(self) -> float:
 
@@ -52,7 +89,7 @@ class HeartModelPulse:
 
     def get_volume(self) -> float:
 
-        return self.compute_volume(self.activation, self.lv_pressure)  # Assumes compute_volume is correct for current state
+        return self.problem.geometry.cavity_volume(u=self.problem.state.sub(0))
 
     
     def save(self, t: float, outname: Path = Path("results.xdmf")):
@@ -126,46 +163,42 @@ class HeartModelPulse:
 
     def apply_bcs(self):
         bcs = pulse.BoundaryConditions(
-            dirichlet=(self._dirichlet_bc,),
+            dirichlet=(self._fixed_base,),
             neumann=self._neumann_bc(),
         )
         return bcs
         
         
         
-    def _dirichlet_bc(self, W):
-        # V = W if W.sub(0).num_sub_spaces() == 0 else W.sub(0)
+    def _fixed_endoring(self, W):
+        V = W if W.sub(0).num_sub_spaces() == 0 else W.sub(0)
         
-        # # Fixing the base in x[0] direction
-        # bc_fixed_based = dolfin.DirichletBC(
-        #     V.sub(0),
-        #     dolfin.Constant(0.0),
-        #     self.geometry.ffun,
-        #     self.geometry.markers["BASE"][0],
-        # )
+        # Fixing the endo ring in all directions to prevent rigid body motion
+        endo_ring_points=self._get_endo_ring()
+        endo_ring_points_x0=np.mean(endo_ring_points[:,0])
+        endo_ring_points_radius=np.sqrt(np.min((endo_ring_points[:,1]**2+endo_ring_points[:,2]**2)))
         
-        # # Fixing the endo ring in all directions to prevent rigid body motion
-        # endo_ring_points=self._get_endo_ring()
-        # endo_ring_points_x0=np.mean(endo_ring_points[:,0])
-        # endo_ring_points_radius=np.sqrt(np.min((endo_ring_points[:,1]**2+endo_ring_points[:,2]**2)))
-        
-        # class EndoRing_subDomain(dolfin.SubDomain):
-        #     def __init__(self, x0, x2):
-        #         super().__init__()
-        #         self.x0 = x0
-        #         self.x2 = x2
-        #         print(x0)
-        #     def inside(self, x, on_boundary):
-        #         return dolfin.near(x[0], self.x0, .01) and dolfin.near(pow(pow(x[1],2)+pow(x[2],2),0.5), self.x2, .1)
+        class EndoRing_subDomain(dolfin.SubDomain):
+            def __init__(self, x0, x2):
+                super().__init__()
+                self.x0 = x0
+                self.x2 = x2
+                print(x0)
+            def inside(self, x, on_boundary):
+                return dolfin.near(x[0], self.x0, .01) and dolfin.near(pow(pow(x[1],2)+pow(x[2],2),0.5), self.x2, .1)
             
-        # endo_ring_fixed=dolfin.DirichletBC(
-        #     V,
-        #     dolfin.Constant((0.0,0.0,0.0)),
-        #     EndoRing_subDomain(endo_ring_points_x0,endo_ring_points_radius),
-        #     method="pointwise",
-        # )
-        # return [bc_fixed_based,endo_ring_fixed]
+        endo_ring_fixed=dolfin.DirichletBC(
+            V,
+            dolfin.Constant((0.0,0.0,0.0)),
+            EndoRing_subDomain(endo_ring_points_x0,endo_ring_points_radius),
+            method="pointwise",
+        )
+        return endo_ring_fixed
 
+        
+    
+    def _fixed_base(self,W):
+        
         V = W if W.sub(0).num_sub_spaces() == 0 else W.sub(0)
         
         # Fixing the base in x[0] direction
@@ -177,8 +210,7 @@ class HeartModelPulse:
         )
         
         return bc_fixed_based
-        
-        
+    
     def _neumann_bc(self):
         # LV Pressure
         lv_marker = self.geometry.markers["ENDO"][0]
@@ -219,14 +251,20 @@ if outname.is_file():
     outname.unlink()
     outname.with_suffix(".h5").unlink()
 
-activation=np.linspace(0,10,10)
-pressure=np.linspace(0,1,10)
+activation=np.linspace(0.1,10,10)
+pressure=np.linspace(0.1,1,10)
 
 model = HeartModelPulse()
 model.compute_volume(0,0)
 model.save(0, outname=outname)
+
+volumes=[]
+dV_dPs=[]
 for t, (a,p) in enumerate(zip(activation,pressure)):
-    model.compute_volume(a,p)
+    v=model.compute_volume(a,p)
+    volumes.append(v)
+    volume_derivation=model.compute_volume_derivation(a,p)
+    dV_dPs.append(volume_derivation)
     model.save(t+1, outname=outname)
     
 # %%
